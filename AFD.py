2fb7ea765ca946c88d3400f6a025382d3831d3b3
Isabel Solano <misolanob@gmail.com>
2022-09-19 12:05:21 -0600

    Mejoras en construcción directa de AFD

--- AFD.py
+++ AFD.py
@@ -9,12 +9,16 @@ from Tree import *
 class AFD():
     def __init__(self):
         self.afd = []
+        self.EA = []
         self.Dstates = []
+        self.Symbols = []
 
     def directConstruction(self, arbol):
 
         # Alfabeto de la expresión regex
-        Symbols = self.detSymbols(arbol)
+        self.Symbols = self.detSymbols(arbol)
+        # eliminamos '#' si lo trae
+        self.Symbols.remove('#')
 
         # Firstpos de la raíz
         cant_n = len(arbol.Arbol)
@@ -23,38 +27,36 @@ class AFD():
 
         Dstates_marked = []
 
+        # [qi, e, qf]
+
         # Si no todos los estados de Dstates están marcados (en Dstates_marked) se continúa
         while (not all(t in Dstates_marked for t in Dstates)):
             for t in Dstates:
                 # Se marca el estado actual
                 Dstates_marked.append(t)
 
-                for a in Symbols:
+                for a in self.Symbols:
                     U = []
-                    if (a != '#'):
-                        # Ignoramos el último #
 
                         # Buscamos en todo el árbol por las posiciones que tengan el símbolo
-                        for x in t:
-                            if arbol.States[x].getSymbol() == a:
-                                # Si no está en el estado, se añade
-                                for e in arbol.States[x].followpos:
-                                    if (e not in U):
-                                        U.append(e)
-                        
-                        if (len(U) > 0):
-                            if (U not in Dstates):
-                                Dstates.append(U)
+                    for x in t:
+                        if arbol.States[x].getSymbol() == a:
+                            # Si no está en el estado, se añade
+                            for e in arbol.States[x].followpos:
+                                if (e not in U):
+                                    U.append(e)
+                    
+                    if (len(U) > 0):
+                        if (U not in Dstates):
+                            Dstates.append(U)
 
-                            self.afd.append([t, a, U])
+                        self.afd.append([t, a, U])
 
-        print('afd: ', self.afd)
+        # Determinamos estados de aceptación
+        self.EA = [(len(arbol.States) - 1) in x for x in Dstates]
 
         # Renombramos los estados 
         newStates = ["q%s"%x for x in range(len(Dstates))]
-        
-        for x in range(len(Dstates)):
-            print(Dstates[x],'\t', newStates[x])
 
         temp_afd = []
         for trans in self.afd:
@@ -66,9 +68,8 @@ class AFD():
                     t.append(element)
             temp_afd.append(t)
 
+        # Remplazamos
         self.afd = temp_afd
-        print('afd: ', self.afd)
-
 
     def simulation(self):
         """"""
@@ -90,11 +91,15 @@ class AFD():
 # pruebas
 #r = 'ab*ab*#'
 #r = '(a|b)*(a|b)*a?#'
-r = '(aa|bb)*#'
+#r = '(aa|bb)*#'
 #r = 'a(a|b)*#'
 #r = '(a|b)|(abab)'
+#r = '0(0|1)0#'
+r = '0*(0*|1)1#'
 arbol = Tree(r)
-print()
-print(arbol)
+#print()
+#print(arbol)
 dfa = AFD()
 dfa.directConstruction(arbol)
+for x in dfa.afd:
+    print(x)
