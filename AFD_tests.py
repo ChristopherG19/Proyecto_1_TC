506af9f2bd49b23bd7c4a53c238f3303c5f084aa
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-09-21 12:08:52 -0600

    Arreglos Tests y eliminación archivos innecesarios

--- AFD_tests.py
+++ AFD_tests.py
@@ -1,6 +1,35 @@
-from pprint import pprint
+# Universidad del Valle de Guatemala
+# Facultad de Ingeniería
+# Departamento de Ciencias de la Computación
+# CC2019 Teoría de la computación
+# Grupo#9 
+
 from AFD import *
+from AFN_Generator import * 
+from SimulacionAFN import *
+from subconjuntos  import *
 from time import perf_counter
+
+# _________________________________________
+# Temporal 
+# _________________________________________
+
+# r = "a"
+# r = "ab"
+# r = "aab"
+# r = "(0|1)"
+# r = "a*"
+# r = "(a|b)*"
+# r = "0(0|1)0"
+# r = "0*(0*|1)1"
+# r = "0*11*0"
+# r = "(b|b)*abb(a|b)*"
+# r = "(a|b)*|(a|b)*"
+# r = "((a|b)|(abab))|a"
+# r = "ab*ab*"
+# r = "(a|b)$"
+r2 = "(a|b)*(abba*|(ab)*ba)"
+w2 = "abbaaa"
 
 # _________________________________________
 # pruebas 
@@ -37,6 +66,39 @@ w = '000000111'
 # 
 # r = '00*1110(0|1*)#'
 # w = '01110111'
+
+# --------------------------------------------------
+# Construcción de AFN con Thompson
+print('\n--> Construcción AFN con construcción de Thompson')
+t1_start = perf_counter()
+Cons = Construction(r2)
+AFN_Thompson = Cons.Thompson_Construction()
+t1_stop = perf_counter()
+Cons.printResults(AFN_Thompson)
+print('Tiempo de ejecución: %.4e ms'%(t1_stop - t1_start))
+# --------------------------------------------------
+
+# --------------------------------------------------
+# Simulación de cadenas en AFN
+print('\n--> Simulación AFN')
+t1_start = perf_counter()
+ResultadoSimulacion = SimulationAFN(r2, w2)
+print("\nExpresion regular:", r2)
+print("Cadena a evaluar:", w2)
+print("Resultado: La cadena %s"%w2, ResultadoSimulacion, "es aceptada\n")
+t1_stop = perf_counter()
+print('Tiempo de ejecución: %.4e ms'%(t1_stop - t1_start))
+# --------------------------------------------------
+
+# --------------------------------------------------
+# Construcción de AFD con subconjuntos
+print('\n--> Construcción AFD con subconjuntos')
+t1_start = perf_counter()
+AFD_Subconjuntos = AFN_To_AFD_SC(r2)
+printResultsAFD(AFD_Subconjuntos[0], AFD_Subconjuntos[1], AFD_Subconjuntos[2])
+t1_stop = perf_counter()
+print('Tiempo de ejecución: %.4e ms'%(t1_stop - t1_start))
+# --------------------------------------------------
 
 # --------------------------------------------------
 # Construcción del árbol sintáctico
